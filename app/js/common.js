58b188b7ad210105e5f67a7de6bed2e0fc205dc9
Alexandr Dulya <sasa97973@yandex.ru>
2017-10-15 21:39:33 +0300

    Slider fade

--- app/js/common.js
+++ app/js/common.js
@@ -37,11 +37,20 @@ $(document).ready(function() {
         slidesToShow: 1,
         slidesToScroll: 1,
         fade: true,
+        speed: 600,
+        cssEase: "ease-in-out",
         autoplay: true,
         autoplaySpeed: 3000,
         pauseOnHover: false,
         nextArrow: "<span class='custom-next'><i class=\"fa fa-chevron-right\" aria-hidden=\"true\"></i></span>",
         prevArrow: "<span class='custom-prev'><i class=\"fa fa-chevron-left\" aria-hidden=\"true\"></i></span>"
+    });
+
+    slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        $(".slider__item[data-slick-index="+nextSlide+"] > img, .slider__item[data-slick-index="+currentSlide+"] > .slider__info").css("opacity", 0);
+        $(".slider__item[data-slick-index="+nextSlide+"] > img, .slider__item[data-slick-index="+currentSlide+"] > .slider__info").animate({
+            opacity: 1
+        }, 1000)
     });
 
     //---------------------
@@ -66,7 +75,7 @@ $(document).ready(function() {
     var itemNested = $(".nav__item-nested");
     function autoHeightMenu() {
         var menu1Bottom = -menu1.outerHeight(false);
-        var menu2Bottom = -menu2.height() + itemNested.outerHeight(false)/2 ;
+        var menu2Bottom = -menu2.height() + itemNested.outerHeight(false);
         var menu2Right = -menu2.outerWidth(false);
         var langBottom = -lang.outerHeight(false);
         menu1.css("bottom", menu1Bottom);
